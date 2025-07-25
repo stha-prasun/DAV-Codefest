8fbf79a1490776fdde3cf440c2df1fad4dfcf309
Prasun <shresthaprasun88@gmail.com>
2025-07-25 09:48:16 +0545

    Minor Fix

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup } from "../controllers/userController";
+import { signup } from "../controllers/userController.js";
 
 const router = express.Router();
 
