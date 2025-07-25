d0d336060c87a94ecb8775a2dcc8b774f3b5f19b
Senpai_Does_Dab <utsavbhattarai29@gmail.com>
2025-07-25 09:04:49 +0545

    done

---
+++ Frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li className="text-2xl">Home</li>
+          <li className="text-2xl">About</li>
+          <li className="text-2xl btn btn-primary">Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
