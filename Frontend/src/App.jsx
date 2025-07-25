37ba33ea764f5177afff5f943284f2208472f144
Senpai_Does_Dab <utsavbhattarai29@gmail.com>
2025-07-25 10:03:10 +0545

    Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from './components/shared/Navbar'
 import HomePage from './components/HomePage'
 import Footer from './components/shared/Footer'
 const App = () => {
   return (
     <div>
-      <Navbar />
       <HomePage />
       <Footer />
     </div>
