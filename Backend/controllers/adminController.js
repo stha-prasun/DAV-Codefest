567808db7ce4a2494dd4d509b721526e145250a5
Prasun <shresthaprasun88@gmail.com>
2025-07-25 09:42:39 +0545

    Admin and User Signup

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,37 +1,36 @@
-import { User } from "../models/userSchema.js";
 import bcrypt from "bcryptjs";
+import { Admin } from "../models/adminSchema.js";
 //import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
   try {
-    const { fullname, email, password } = req.body;
+    const { username, password } = req.body;
 
-    if (!fullname || !email || !password) {
+    if (!username || !password) {
       return res.status(400).json({
-        message: "All Fields Required",
+        message: "Please provide username and password",
         success: false,
       });
     }
 
-    const user = await User.find({ email });
+    const user = await Admin.find({ username });
 
     if (user) {
       return res.status(400).json({
-        message: "User with email already exists with this email",
+        message: "User already exists",
         success: false,
       });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await User.create({
-      fullname,
+    await Admin.create({
       email,
       password: hashedPassword,
     });
 
     return res.status(201).json({
-      message: "User created successfully!!",
+      message: "Admin created successfully!!",
       success: true,
     });
   } catch (error) {
