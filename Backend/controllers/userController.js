3265d7ca5a0f26d401608aa66d758d7ad0b4c71f
Prasun <shresthaprasun88@gmail.com>
2025-07-25 10:19:42 +0545

    User and Admin Login Features Added

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import { User } from "../models/userSchema.js";
 import bcrypt from "bcryptjs";
-//import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
   try {
@@ -34,6 +34,66 @@ export const signup = async (req, res) => {
       message: "User created successfully!!",
       success: true,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Fields cannot be left empty!!",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid User",
+        success: false,
+      });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      return res.status(400).json({
+        message: "Incorrect Email or Password",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userID: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    const loggedInUser = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+    };
+
+    return res
+      .status(201)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome Back ${user.fullname}!!`,
+        success: true,
+        loggedInUser,
+      });
   } catch (error) {
     console.log(error);
   }
--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { admin_signup } from "../controllers/adminController.js";
+import { admin_login, admin_signup } from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.route("/signup").post(admin_signup);
+
+router.route("/login").post(admin_login);
 
 export default router;
