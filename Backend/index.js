47af2ac788955d2cbb88d7e64bd8a70a34752339
Prasun <shresthaprasun88@gmail.com>
2025-07-25 09:17:28 +0545

    Server Connected

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./config/database.js"
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+const app = express();
+
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOption));
+
+//Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server running on port ${port}`);
+});
