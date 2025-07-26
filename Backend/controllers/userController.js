df35500f6fe63ba5e8425bfac12357be4a96dd17
Prasun <shresthaprasun88@gmail.com>
2025-07-26 09:24:34 +0545

    Fix

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -13,9 +13,9 @@ export const signup = async (req, res) => {
       });
     }
 
-    const user = await User.find({ email });
+    const user = await User.findOne({ email });
 
-    if (!user) {
+    if (user) {
       return res.status(400).json({
         message: "User with email already exists with this email",
         success: false,
