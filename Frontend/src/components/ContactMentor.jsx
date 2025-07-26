37b590bb4a4147f5037c0f3aa7d7f7aeac210da1
Prasun <shresthaprasun88@gmail.com>
2025-07-26 10:51:27 +0545

    Send Message

--- Frontend/src/components/ContactMentor.jsx
+++ Frontend/src/components/ContactMentor.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { TbArrowBackUp } from "react-icons/tb";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { MESSAGE_API_ENDPOINT } from "../utils/constants";
 
 const ContactMentor = () => {
+  const loggedInUser = useSelector((store)=>store?.User?.loggedInUser);
+
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // HANDLE SUBMIT FUNCTION
+  const handleSend = async () => {
+    if (!message.trim()) {
+      toast.error("Message cannot be empty.");
+      return;
+    }
+    setLoading(true);
+
+    try {
+      const response = await axios.post(`${MESSAGE_API_ENDPOINT}/new`, {
+        message,
+        id: loggedInUser?._id,
+      }, {withCredentials: true});
+
+      if (response.data?.success) {
+        toast.success(response.data.message);
+        setMessage("");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div
       className="relative min-h-screen flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat overflow-hidden"
@@ -37,13 +72,19 @@ const ContactMentor = () => {
             <textarea
               placeholder="Write your message here"
               className="w-full resize-none rounded-xl border border-gray-300 bg-neutral-50 text-[#141414] p-4 h-36 text-base placeholder:text-neutral-500 focus:outline-none focus:ring-1 focus:ring-black"
+              value={message} 
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </label>
         </div>
 
         <div className="px-2 text-left">
-          <button className="px-5 py-2 rounded-xl bg-black text-white font-semibold text-sm hover:bg-gray-900 transition cursor-pointer">
-            Send
+          <button
+            onClick={handleSend}
+            className="px-5 py-2 rounded-xl bg-black text-white font-semibold text-sm hover:bg-gray-900 transition cursor-pointer"
+            disabled={loading}
+          >
+            {loading ? "Sending..." : "Send"}
           </button>
         </div>
       </div>
--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -3,3 +3,5 @@ export const USER_API_ENDPOINT = "http://localhost:8080/api/v1/user";
 export const COURSE_API_ENDPOINT = "http://localhost:8080/api/v1/course";
 
 export const QUESTIONS_API_ENDPOINT = "http://localhost:8080/api/v1/questions";
+
+export const MESSAGE_API_ENDPOINT = "http://localhost:8080/api/v1/message";
